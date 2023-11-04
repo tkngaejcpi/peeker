0d64826110699e0ce8503ef6ff12b9df5d3deacb
Riley Chang <rileycki3333@gmail.com>
2023-11-04 18:09:58 +0800

    add a loaded reminder in entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { Peeker } from "@elements/Peeker";
 import { Peekable } from "@elements/Peekable";
+import { Peeker } from "@elements/Peeker";
 
+/* place holder */
 export default {
   Peekable,
   Peeker,
 };
+
+/* show a log to remind */
+console.info(
+  "module 'peeker' is loaded, you can use custom elements 'vvv-peekable' and 'vvv-peeker' now!"
+);
+
+console.info(
+  "you can know more about peeker at https://github.com/tkngaejcpi/peeker."
+);
